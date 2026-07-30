23c8b2b6eb6dac94560acf9efa4a503c0ef13abd
Athul Krishna K <athulprgm.pycslearn@gmail.com>
2026-07-30 13:16:24 +0530

    Fix

--- app/Models/SupportTicket.php
+++ app/Models/SupportTicket.php
@@ -2,14 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-class SupportTicket extends Model
-{
-    //
-}
-namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
